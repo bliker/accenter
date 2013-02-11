313d107f1eb63dcea4863ba4f1a93155ec967f29
Samuel Vasko <samvasko@gmail.com>
2013-02-11 15:02:20 +0100

    modified to be proper jquery extension

--- accenter.js
+++ accenter.js
@@ -1,42 +1,74 @@
 /*
- *  Project: Accenter
- *  Description: Shams diacritical marks to fonts that don't support them
- *  Author: Samuel Vasko
- *  License: DBAD
- */
+*  Project: Accenter
+*  Description: Shams diacritical marks to fonts that don't support them
+*  Author: Samuel Vasko
+*  License: DBAD
+*/
 
-// scripts and/or other plugins which may not be closed properly.
 ;(function ( $, window, undefined ) {
 
-  // Create the defaults once
-  var pluginName = 'accenter',
-      document = window.document,
-      defaults = {
-        propertyName: "value"
-      };
+// Create the defaults once
+var pluginName = 'accenter',
+document = window.document,
+defaults = {
+	propertyName: "value"
+};
 
-  // The actual plugin constructor
-  function Plugin( element, options ) {
-    this.element = element;
+// The actual plugin constructor
+function Plugin( element, options ) {
+	this.element = element;
+	this.options = $.extend( {}, defaults, options) ;
+	this._defaults = defaults;
+	this._name = pluginName;
+	this.init();
+}
 
-    this.options = $.extend( {}, defaults, options) ;
+Plugin.prototype.init = function () {
+	// fetch input text
+	var inputText  = $(this.element).text(),
+		modified = '';
 
-    this._defaults = defaults;
-    this._name = pluginName;
-    this.init();
-    this.text = this.text();
-  }
+	// loop thru each letter
+	for (var i = 0; i < inputText.length; i++) {
+		var input = inputText[i],
+			alt = letters[input.toLowerCase()],  // find it there is a unaccented alternative =
+			output = input,
+			elClass = ''; // class that will be applied to separated character
+			upper = false;
 
-  Plugin.prototype.init = function () {
+			// if unaccented alternative extis
+			if ( typeof alt !== 'undefined' ) {
+				output = alt['alt'];
+				elClass = alt['type'];
+				tall = input == input.toUpperCase();
 
-  };
+				// tall letters that require specal caron
+				if ( (output == 'l' |  output == 't') && !upper ) {
+					elClass += ' tall';
+				}
+				if ( output == 'd' && !upper ) {
+					elClass += ' talld';
+				}
+				if ( upper )
+				{
+					elClass += ' upper';
+					output = output.toUpperCase();
+				}
+				// create element containing that letter
+				output = '<span class="'+ elClass +'">'+output+'</span>';
+				inputText[i] = output;
+			}
+			modified += output;
+	}
+	$(this.element).html(modified).css({'visibility': 'visible'});
+};
 
-  $.fn[pluginName] = function ( options ) {
-    return this.each(function () {
-      if (!$.data(this, 'plugin_' + pluginName)) {
-        $.data(this, 'plugin_' + pluginName, new Plugin( this, options ));
-      }
-    });
-  };
+$.fn[pluginName] = function ( options ) {
+	return this.each(function () {
+		if (!$.data(this, 'plugin_' + pluginName)) {
+			$.data(this, 'plugin_' + pluginName, new Plugin( this, options ));
+		}
+	});
+};
 
 }(jQuery, window));
